2c7d7c3de304a2aca1e13e7f2647fe6ff93b5906
Marijke Luttekes <mail@marijkeluttekes.dev>
2025-05-12 22:19:03 +0200

    Add a kitchen sink page

--- src/mysite/core/urls.py
+++ src/mysite/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from mysite.core.error_views import raise_error
-from mysite.core.views import HomeView
+from mysite.core.views import HomeView, KitchenSinkView
 
 
 app_name = "core"
 urlpatterns = [
     path("error/<int:http_status>/", raise_error, name="raise_error"),
+    path("kitchen-sink/", KitchenSinkView.as_view(), name="kitchen_sink"),
     path("", HomeView.as_view(), name="home"),
 ]
--- src/mysite/core/views.py
+++ src/mysite/core/views.py
@@ -12,3 +12,9 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["http_error_codes"] = [400, 403, 404, 500]
         return context
+
+
+class KitchenSinkView(TemplateView):
+    """HTML element showcase view (kitchen sink)."""
+
+    template_name = "core/pages/kitchen-sink/index.html"
