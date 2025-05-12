5dbd9f53327188dae655555e191fb4e5c148546d
Marijke Luttekes <mail@marijkeluttekes.dev>
2025-05-12 20:46:05 +0200

    Link to error views in homepage

--- src/mysite/core/views.py
+++ src/mysite/core/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.views.generic import TemplateView
 
 
@@ -5,3 +7,8 @@ class HomeView(TemplateView):
     """Website home view (index)."""
 
     template_name = "core/pages/home/index.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:  # noqa: D102
+        context = super().get_context_data(**kwargs)
+        context["http_error_codes"] = [400, 403, 404, 500]
+        return context
