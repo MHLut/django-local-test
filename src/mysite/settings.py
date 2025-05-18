48609dad8f7a3ff481d358836728fec7ca5b224e
Marijke Luttekes <mail@marijkeluttekes.dev>
2025-05-18 16:29:48 +0200

    Add static and media settings

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -164,4 +164,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "/static/"
+
+
+# Media files
+# https://docs.djangoproject.com/en/4.2/topics/files/
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
