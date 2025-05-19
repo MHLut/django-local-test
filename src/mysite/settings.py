2de275b5fce3a340f7f2b281da5c0cf4f4e6e608
Marijke Luttekes <mail@marijkeluttekes.dev>
2025-05-19 16:12:53 +0200

    Add LocaleMiddleware

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     "silk.middleware.SilkyMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
