c7adf4627f57aa7e06b94dc313054663c63ee9d2
Marijke Luttekes <mail@marijkeluttekes.dev>
2026-09-03 15:26:05 +0200

    feat: add read-only full list admin view for TodoList

--- src/mysite/todo/admin.py
+++ src/mysite/todo/admin.py
@@ -1,7 +1,12 @@
 from typing import TYPE_CHECKING
 
 from django.contrib import admin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http.request import HttpRequest
+from django.urls import path
+from django.utils.text import capfirst
 from django.utils.translation import pgettext_lazy
+from django.views.generic.detail import DetailView
 
 from mysite.todo.models import TodoItem, TodoList
 
@@ -17,6 +22,34 @@ class TodoItemInline(admin.TabularInline):
 
     model = TodoItem
     extra = 1
+
+
+class TodoListFullDetailView(PermissionRequiredMixin, DetailView):
+    """A read-only admin page showing a list and all its items."""
+
+    permission_required = "todo.view_todolist"
+    http_method_names = ["get"]  # noqa: RUF012
+
+    model = TodoList
+    context_object_name = "todo_list"
+    template_name = "admin/todo/todolist/full.html"
+
+    def get_context_data(self, **kwargs):  # noqa: D102
+        opts = self.model._meta
+        title = pgettext_lazy(
+            "Admin page title, TodoList",
+            "Full %(verbose_name)s: %(list_title)s"  # noqa: UP031
+            % {"verbose_name": opts.verbose_name, "list_title": self.object.title},
+        )
+
+        return {
+            **super().get_context_data(**kwargs),
+            **admin.site.each_context(self.request),
+            "title": title,
+            "subtitle": None,
+            "module_name": str(capfirst(opts.verbose_name_plural)),
+            "opts": opts,
+        }
 
 
 @admin.register(TodoList)
@@ -70,3 +103,14 @@ class TodoListAdmin(admin.ModelAdmin):
         """Display the percentage of finished items in a list."""
         precision = 1 if obj.progress % 1 > 0 else 0
         return f"{obj.progress:.{precision}f}%"
+
+    def get_urls(self):
+        """Add custom `Full list` view."""
+        return [
+            path(
+                "<pk>/full/",
+                self.admin_site.admin_view(TodoListFullDetailView.as_view()),
+                name="todo_todolist_full",
+            ),
+            *super().get_urls(),
+        ]
--- src/mysite/todo/tests/test_admin_views.py
+++ src/mysite/todo/tests/test_admin_views.py
@@ -20,6 +20,12 @@ class TodoListAdminViewsTestCase(TestCase):
             is_superuser=True,
             is_active=True,
         )
+        self.regular_user = get_user_model().objects.create(
+            username="regular_user",
+            is_staff=False,
+            is_superuser=False,
+            is_active=True,
+        )
 
         self.list = TodoList.objects.create(title="Testing tasks")
         TodoItem.objects.create(todo_list=self.list, description="One")
@@ -34,6 +40,9 @@ class TodoListAdminViewsTestCase(TestCase):
             ("admin:todo_todolist_delete", {"object_id": pk}),
             ("admin:todo_todolist_history", {"object_id": pk}),
         ]
+        self.custom_view_paths = [
+            ("admin:todo_todolist_full", {"pk": pk}),
+        ]
 
     def test_default_views_load(self):
         """Check if default admin views load without errors."""
@@ -45,3 +54,25 @@ class TodoListAdminViewsTestCase(TestCase):
                 response = self.client.get(path)
 
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_custom_views_load(self):
+        """Check if custom admin views load without errors."""
+        self.client.force_login(self.admin_user)
+
+        for path_info in self.custom_view_paths:
+            with self.subTest(path_name=path_info[0]):
+                path = reverse(path_info[0], kwargs=path_info[1])
+                response = self.client.get(path)
+
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_custom_views_are_protected(self):
+        """Check if non-admin user gets redirected away from custom admin views."""
+        self.client.force_login(self.regular_user)
+
+        for path_info in self.custom_view_paths:
+            with self.subTest(path_name=path_info[0]), suppress(MissingVariableError):
+                path = reverse(path_info[0], kwargs=path_info[1])
+                response = self.client.get(path)
+
+                self.assertEqual(response.status_code, HTTPStatus.FOUND)
